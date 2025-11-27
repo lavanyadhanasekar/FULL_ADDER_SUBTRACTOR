7a7bceb2e7314adcf36147c7139b1f0ead26c734
lavanyadhanasekar <lavanyadhana398@gmail.com>
2025-11-27 21:11:43 +0530

    Add files via upload

---
+++ lab5.v
@@ -0,0 +1,12 @@
+module lab5 (
+    input  wire a, b, cin,   // Inputs
+    output wire sum, carry   // Outputs
+);
+
+    // Logic equations
+    assign sum   = a ^ b ^ cin;                  // XOR for sum
+    assign carry = (a & b) | (b & cin) | (a & cin); // Majority function for carry
+
+endmodule
+
+
